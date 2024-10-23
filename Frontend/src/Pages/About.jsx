89004b945f0b23372a3f3957f2bfe43d535b95c0
Satyam Patil <87993745+Senku6969@users.noreply.github.com>
2024-10-23 17:34:49 +0530

    added frontend mainly react?

---
+++ Frontend/src/Pages/About.jsx
@@ -0,0 +1,35 @@
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPages";
+import PlanTrip from "../components/PlanTrip";
+import AboutMain from "../images/about/about-main.jpg";
+import Box1 from "../images/about/icon1.png";
+import Box2 from "../images/about/icon2.png";
+import Box3 from "../images/about/icon3.png";
+
+function About() {
+  return (
+    <>
+      <section className="about-page">
+        <HeroPages name="About" />
+        <div className="container">
+          <PlanTrip />
+        </div>
+      </section>
+      <div className="book-banner">
+        <div className="book-banner__overlay"></div>
+        <div className="container">
+          <div className="text-content">
+            <h2>Book a car by getting in touch with us</h2>
+            <span>
+              <i className="fa-solid fa-phone"></i>
+              <h3>123-456-7869</h3>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default About;
